115b128cd10f64a0d1b16d71941d454c0f1f4b75
stefanJoseph5671 <50321310+stefanJoseph5671@users.noreply.github.com>
2023-12-11 22:04:27 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.slider-container').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     infinite: true,
     fade: true,
@@ -38,6 +38,18 @@ $(document).ready(function () {
     columnWidth: 200
   });
 });
+
+scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+var myScrollFunc = function () {
+  var y = window.scrollY;
+  if (y >= 150) {
+    scrollToTopBtn.className = "scroll-to-top-btn show-btn";
+  } else {
+    scrollToTopBtn.className = "scroll-to-top-btn hide-btn";
+  }
+};
+window.addEventListener("scroll", myScrollFunc);
 
 function featuredLinkClick(linkId) {
   switch (linkId) {
@@ -105,3 +117,112 @@ function featuredLinkClick(linkId) {
       break;
   }
 }
+
+function openMenu() {
+  document.getElementById("sideNav").style.display = 'flex';
+  document.getElementById("menuClose").style.display = 'block';
+  document.getElementById("menuOpen").style.display = 'none';
+}
+
+function closeMenu() {
+  document.getElementById("sideNav").style.display = 'none';
+  document.getElementById("menuClose").style.display = 'none';
+  document.getElementById("menuOpen").style.display = 'block';
+}
+
+function projectsLinkClick(linkId) {
+  switch (linkId) {
+    case 1:
+      document.getElementById("residentialProjects").classList.remove("projects-link-active");
+      document.getElementById("commercialProjects").classList.remove("projects-link-active");
+      document.getElementById("specialProjects").classList.remove("projects-link-active");
+      document.getElementById("interiorProjects").classList.remove("projects-link-active");
+      document.getElementById("allProjects").classList.add("projects-link-active");
+      document.getElementById("residentialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("commercialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("specialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("interiorProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("allProjectsGrid").classList.add("active-projects-grid");
+      break;
+    case 2:
+      document.getElementById("allProjects").classList.remove("projects-link-active");
+      document.getElementById("commercialProjects").classList.remove("projects-link-active");
+      document.getElementById("specialProjects").classList.remove("projects-link-active");
+      document.getElementById("interiorProjects").classList.remove("projects-link-active");
+      document.getElementById("residentialProjects").classList.add("projects-link-active");
+      document.getElementById("allProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("commercialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("specialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("interiorProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("residentialProjectsGrid").classList.add("active-projects-grid");
+      break;
+    case 3:
+      document.getElementById("allProjects").classList.remove("projects-link-active");
+      document.getElementById("residentialProjects").classList.remove("projects-link-active");
+      document.getElementById("specialProjects").classList.remove("projects-link-active");
+      document.getElementById("interiorProjects").classList.remove("projects-link-active");
+      document.getElementById("commercialProjects").classList.add("projects-link-active");
+      document.getElementById("allProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("residentialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("specialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("interiorProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("commercialProjectsGrid").classList.add("active-projects-grid");
+      break;
+    case 4:
+      document.getElementById("allProjects").classList.remove("projects-link-active");
+      document.getElementById("residentialProjects").classList.remove("projects-link-active");
+      document.getElementById("commercialProjects").classList.remove("projects-link-active");
+      document.getElementById("interiorProjects").classList.remove("projects-link-active");
+      document.getElementById("specialProjects").classList.add("projects-link-active");
+      document.getElementById("allProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("residentialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("commercialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("interiorProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("specialProjectsGrid").classList.add("active-projects-grid");
+      break;
+    case 5:
+      document.getElementById("allProjects").classList.remove("projects-link-active");
+      document.getElementById("residentialProjects").classList.remove("projects-link-active");
+      document.getElementById("commercialProjects").classList.remove("projects-link-active");
+      document.getElementById("specialProjects").classList.remove("projects-link-active");
+      document.getElementById("interiorProjects").classList.add("projects-link-active");
+      document.getElementById("allProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("residentialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("commercialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("specialProjectsGrid").classList.remove("active-projects-grid");
+      document.getElementById("interiorProjectsGrid").classList.add("active-projects-grid");
+      break;
+    default:
+      break;
+  }
+}
+
+function goToCargo() {
+  window.location.href = "cargo.html";
+}
+
+function goToSangawasa() {
+  window.location.href = "sangawasa.html";
+}
+
+function goToMagalwewa() {
+  window.location.href = "magalwewa.html";
+}
+
+function goToTropical() {
+  window.location.href = "tropical.html";
+}
+
+function goToHilltop() {
+  window.location.href = "hilltop.html";
+}
+
+function goToBudget() {
+  window.location.href = "budget.html";
+}
+
+function goToPractice() {
+  window.location.href = "practice.html";
+}
+
+
