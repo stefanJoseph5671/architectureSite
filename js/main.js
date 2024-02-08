b9e9cb27ecf2d918258b61b5b1d6b15a6976ddae
stefanJoseph5671 <50321310+stefanJoseph5671@users.noreply.github.com>
2024-02-08 14:56:25 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.slider-container').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     infinite: true,
     fade: true,
